2c36cf3bcae8e57e83e7e35a1e03ef6a1ccce0af
iTwicKz <twickes32@ufl.edu>
2017-11-06 14:54:10 -0500

    Moved from deprecated VRFSG-Global-Construction

---
+++ Assets/DrawCircles.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var drawCircle : GameObject;
+
+function Start () {
+	
+}
+
+function Update () {
+	//thiccness can be changed by resizing the "Circle" prefab
+	var fwd = transform.TransformDirection(Vector3.forward);
+	var hit : RaycastHit;
+	var distance = 100; //max distance from camera you can draw
+	var destination = Camera.main.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, 10));
+
+//	if (Input.GetMouseButton(0) && Physics.Raycast(transform.position, fwd, hit, distance) && transform.hasChanged){ 
+//		Instantiate(drawCircle, hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal));
+//		transform.hasChanged = false;
+//	}
+	if (Input.GetMouseButton(0) && transform.hasChanged){ 
+		Instantiate(drawCircle, destination, Quaternion.identity);
+		transform.hasChanged = false;
+	}
+}
